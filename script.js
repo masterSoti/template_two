84b7b923f257521c0db7b618552a55d3f50506b9
ssoti2201 <ssoti2201@bvsd.org>
2014-09-14 00:06:32 -0600

    more styling

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ $(".rslides").responsiveSlides({
 	auto:true,
 	speed:4000,
 	pager:true,
-	nav:true,
+	nav:true
 });
 //reegular Jquery
+$(document).ready(
+	function()
+	{
+		$('img').click(
+			function()
+			{
+				$('body, html').animate({scrollTop: '800px'}, 900);
+			}
+		);
+	}
+);
 //regular Javascript
