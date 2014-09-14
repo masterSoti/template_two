247c88a9c1f540f1e681487514456ae2b2d2d5d3
ssoti2201 <ssoti2201@bvsd.org>
2014-09-14 00:57:09 -0600

    Scroll down function working but the slide show is not

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-//rslides Jquery
-$(".rslides").responsiveSlides({
-	auto:true,
-	speed:4000,
-	pager:true,
-	nav:true
-});
-//reegular Jquery
+//regular Jquery
+//the scroll functions
 $(document).ready(
 	function()
-	{
-		$('img').click(
+	{ 
+		$('#home_img_one').click(
 			function()
 			{
-				$('body, html').animate({scrollTop: '800px'}, 900);
+				$('body, html').animate({scrollTop: '675px'}, 500);
 			}
 		);
 	}
 );
-//regular Javascript
+$(document).ready(
+	function()
+	{ 
+		$('#home_img_two').click(
+			function()
+			{
+				$('body, html').animate({scrollTop: '1150px'}, 500);
+			}
+		);
+	}
+);
+$(document).ready(
+	function()
+	{ 
+		$('#home_img_three').click(
+			function()
+			{
+				$('body, html').animate({scrollTop: '1575px'}, 500);
+			}
+		);
+	}
+);
+$(document).ready(
+	function()
+	{ 
+		$('#home_img_four').click(
+			function()
+			{
+				$('body, html').animate({scrollTop: '2200px'}, 500);
+			}
+		);
+	}
+);
+//regular Java script
+/**/
