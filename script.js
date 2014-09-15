98e190b2a54610120ab77486a60eb00beb092e53
ssoti2201 <ssoti2201@bvsd.org>
2014-09-15 01:11:26 -0600

    still has to be more efficient

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+//rslide jquery
+$(function(){
+	$(".rslides").responsiveSlides({
+  		auto: true,             
+  		speed: 500,           
+  		timeout: 4000,          
+  		nav:true  
+	});
+});
 //regular Jquery
-$(document).ready(
+/*$(document).ready(
 	function(){
 		$('.rslides ul img').clic(
 			function(){
 				var id = $(this).attr('id').split('-')[1];
-				console.log(id);
 				var distance = $('.home-'+ id).offset();
 				$('body, html').animate({scrollTop: distance.height}, 500);
 			}
 		);
 	}
-);
+);*/
 //the scroll functions
-/*$(document).ready(
+$(document).ready(
 	function()
 	{ 
-		$('#home_img_one').click(
+		$('#home_img-pants').click(
 			function()
 			{
 				$('body, html').animate({scrollTop: '725px'}, 500);
@@ -26,7 +34,7 @@ $(document).ready(
 $(document).ready(
 	function()
 	{ 
-		$('#home_img_two').click(
+		$('#home_img-shirts').click(
 			function()
 			{
 				$('body, html').animate({scrollTop: '1200px'}, 500);
@@ -37,7 +45,7 @@ $(document).ready(
 $(document).ready(
 	function()
 	{ 
-		$('#home_img_three').click(
+		$('#home_img-accessories').click(
 			function()
 			{
 				$('body, html').animate({scrollTop: '1625px'}, 500);
@@ -48,7 +56,7 @@ $(document).ready(
 $(document).ready(
 	function()
 	{ 
-		$('#home_img_four').click(
+		$('#home_img-suits').click(
 			function()
 			{
 				$('body, html').animate({scrollTop: '2250px'}, 500);
@@ -56,16 +64,11 @@ $(document).ready(
 		);
 	}
 );
-*/
 //still have to make the pic like the one on google web store
-//rslide jquery
-$(function(){
-	$(".rslides").responsiveSlides({
-  		auto: true,             
-  		speed: 500,           
-  		timeout: 4000,          
-  		nav:true  
-	});
-});
-
+$(document).ready(
+	function()
+	{
+		$('#home_fade_in').slideIn();
+	}
+);
 //regular Java script
