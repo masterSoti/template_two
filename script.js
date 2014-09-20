a57ae02cab0744855341d27b0ab54677b958335d
ssoti2201 <ssoti2201@bvsd.org>
2014-09-19 22:52:19 -0600

    fixed the scroll down buggit add .git add .

--- script.js
+++ script.js
@@ -7,63 +7,18 @@ $(function(){
   		nav:true  
 	});
 });
-//regular Jquery
+//the scroll function
 $(document).ready(
 	function(){
 		$('.rslides ul img').click(
 			function(){
 				var id = $(this).attr('id').split('-')[1];
 				var distance = $('.home-'+ id).offset();
-				$('html').animate({scrollTop: distance.height}, 500);
-			}
-		);
-	}
-);/*
-//the scroll functions
-$(document).ready(
-	function()
-	{ 
-		$('#home_img-pants').click(
-			function()
-			{
-				$('body, html').animate({scrollTop: '725px'}, 500);
+				$('html, body').animate({scrollTop: distance.top}, 500);
 			}
 		);
 	}
 );
-$(document).ready(
-	function()
-	{ 
-		$('#home_img-shirts').click(
-			function()
-			{
-				$('body, html').animate({scrollTop: '1200px'}, 500);
-			}
-		);
-	}
-);
-$(document).ready(
-	function()
-	{ 
-		$('#home_img-accessories').click(
-			function()
-			{
-				$('body, html').animate({scrollTop: '1625px'}, 500);
-			}
-		);
-	}
-);
-$(document).ready(
-	function()
-	{ 
-		$('#home_img-suits').click(
-			function()
-			{
-				$('body, html').animate({scrollTop: '2250px'}, 500);
-			}
-		);
-	}
-);*/ 
 //still have to make the pic like the one on google web store
 $(document).ready(
 	function()
